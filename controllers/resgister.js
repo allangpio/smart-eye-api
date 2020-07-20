c456cf99d2b7a3686a878b5e43175dd1de124887
allangpio <gpioallan@gmail.com>
2020-07-19 22:04:58 -0300

    debug4

--- controllers/resgister.js
+++ controllers/resgister.js
@@ -1,6 +1,5 @@
 const handleRegister = (db, bcrypt) => (req, res) => {
   const {email, name, password} = req.body;
-  console.log(req.body, email, name, password);
   if (!email || !name || !password) {
     return res.status(400).json('incorrect form submission');
   }
@@ -27,10 +26,7 @@ const handleRegister = (db, bcrypt) => (req, res) => {
       })
       .then(trx.commit)
       .catch(trx.rollback);
-  }).catch((err) => {
-    console.log(err);
-    res.status(400).json('Unable to register');
-  });
+  }).catch((err) => res.status(400).json('Unable to register'));
 };
 
 module.exports = {
